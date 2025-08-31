aa7e36aecb59c7237e3dfcc0056dd0d02c8fb077
Aysla <aysladev@gmail.com>
2025-08-31 01:36:29 -0300

    Add files via upload

---
+++ arquivos/js/home/jsons/see-this-cards-json.js
@@ -0,0 +1,18 @@
+const seeThisCardJson = [
+  {
+    id: 1,
+    tag: "Anúncios",
+    cardImage: "images/seeimages/logozoom.jpeg",
+    title: "Resultado dos Concursos",
+    altCard: "Coisa do zoom",
+    slug: "trava_lingua",
+  }, 
+  { 
+    id: 2,
+    tag: "Eventos",
+    cardImage: "images/seeimages/pnae.webp",
+    title: "III Roda de Conversa sobre o PNAE reúne IFs em debate sobre educação alimentar",
+    altCard: "PNAE",
+    slug: "hora_do_conto_por_larissa",
+  },
+];
